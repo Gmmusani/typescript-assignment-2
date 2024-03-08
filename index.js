ab678ead88d9ee3beac55e9cc41c5ec0c0030ec0
ghulam <gulammustafa2930@gmail.com>
2024-03-08 15:10:32 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+"use strict";
+////   variables   ////
+// let
+// const
+// var
+////   data types   ////
+// There are seven data types:
+// string,number,boolean,null,undefined,bigint,symbol.
+// string :
+let a = "Ghulam Mustafa";
+console.log(a);
+// number :
+let b = 123;
+console.log(b);
+// boolean :
+let isFollow = false;
+console.log(isFollow);
+// null :
+let empty = null;
+console.log(empty);
+// undefined :
+let e;
+console.log(e);
+// // bigint:
+// const bigIntValue : bigint = 1234567890123456789012345678901234567890n;
+// console.log(bigIntValue);
+// // symbol :
+// const mySym = symbol("hello");
+// console.log(mySym);
+////   CASE TYPE   ////
+// there are three types of cases :
+// camelCase :
+let fullName = "Musani";
+console.log(fullName);
+// snake_case :
+let full_name = "ausaf";
+console.log(full_name);
+// PascalCase :
+let FullName = "hamza";
+console.log(FullName);
+////  ARRAY   ////
+// Basic syntax ====> let variableName : data type[] = [value];
+// in array index starts from zero.
+let studentAge = [12, 14, 18];
+console.log(studentAge);
+console.log(studentAge[2]);
+let colorName = ["white", "black", "blue"];
+colorName[2] = "orange";
+console.log(colorName);
---
+++ index.ts
@@ -0,0 +1,84 @@
+////   variables   ////
+
+// let
+// const
+// var
+
+////   data types   ////
+
+// There are seven data types:
+
+// string,number,boolean,null,undefined,bigint,symbol.
+
+// string :
+let a : string = "Ghulam Mustafa";
+console.log(a);
+
+// number :
+let b : number = 123;
+console.log(b);
+
+// boolean :
+let isFollow : boolean = false
+console.log(isFollow);
+
+// null :
+let empty : null = null
+console.log(empty);
+
+// undefined :
+let e : undefined
+console.log(e);
+
+// bigint:
+const bigIntValue : bigint = 1234567890123456789012345678901234567890n;
+console.log(bigIntValue);
+
+// symbol :
+const mySym = symbol("hello");
+console.log(mySym);
+
+////   CASE TYPE   ////
+
+// there are three types of cases :
+
+// camelCase :
+let fullName : string = "Musani";
+console.log(fullName);
+
+// snake_case :
+let full_name : string = "ausaf";
+console.log(full_name);
+
+// PascalCase :
+let FullName : string = "hamza";
+console.log(FullName);
+
+////  ARRAY   ////
+
+// Basic syntax ====> let variableName : data type[] = [value];
+
+// in array index starts from zero.
+
+let studentAge : number[] = [12 , 14 , 18];
+console.log(studentAge);
+console.log(studentAge[2]);
+
+let colorName : string[] = ["white" ,"black" , "blue"];
+colorName[2] = "orange"; 
+console.log(colorName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
